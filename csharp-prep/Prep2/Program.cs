83177f77c80d601cbee75f12bb25ccb2efc9a3bb
wallylime <82895781+wallylime@users.noreply.github.com>
2023-01-05 12:14:25 -0700

    first two core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        
+        if (grade >= 90)
+        {
+            Console.WriteLine("You have an A. Good job!");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You have a B. Pretty good job!");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You have a C. You're a very average student.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You have a D. You need to work harder.");
+        }
+        else
+        {
+            Console.WriteLine("You have an F. Yikes!");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Good job passing the class!");
+        }
+
+        else 
+        {
+            Console.WriteLine("Do you really want to fail this class? Work harder!");
+        }
     }
 }
