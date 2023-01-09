8dc18d15ae3b882ad2f49a56cb88d2f088f20c8f
wallylime <82895781+wallylime@users.noreply.github.com>
2023-01-09 13:06:38 -0700

    Step 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);//I originally had .Next(1, 100) but noticed in the solution that it was .Next(1, 101) so it must not be inclusive of the last number
         int guess;
+        Console.WriteLine("Guess a number from 1 to 100!");
         do {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
