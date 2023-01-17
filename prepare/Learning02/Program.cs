cb598781cd4b5079edcb1fa1ff7f10e073f1ffe6
wallylime <82895781+wallylime@users.noreply.github.com>
2023-01-17 11:31:20 -0700

    added job class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Sales";
+        job1._company = "Slimy Car Dealership";
+        job1._startYear = 2000;
+        job1._endYear = 2010;
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Customer Service";
+        job2._company = "Angry Customers Inc.";
+        job2._startYear = 2010;
+        job2._endYear = 2014;
+        job2.DisplayJobDetails();
     }
 }
