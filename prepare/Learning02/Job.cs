f91c10fa2367c520e034d86c71288895a9aa1911
wallylime <82895781+wallylime@users.noreply.github.com>
2023-01-17 13:21:53 -0700

    finished assignment

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Job
 {
   public string _company = "";
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,13 +9,17 @@ class Program
         job1._company = "Slimy Car Dealership";
         job1._startYear = 2000;
         job1._endYear = 2010;
-        job1.DisplayJobDetails();
 
         Job job2 = new Job();
         job2._jobTitle = "Customer Service";
         job2._company = "Angry Customers Inc.";
         job2._startYear = 2010;
         job2._endYear = 2014;
-        job2.DisplayJobDetails();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Acme Person";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.DisplayResume();
     }
 }
