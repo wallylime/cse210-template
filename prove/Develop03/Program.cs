a73e501f5e51b1e4a3a8ba493ea4da3a73213aac
wallylime <82895781+wallylime@users.noreply.github.com>
2023-02-09 09:44:20 -0700

    all I want in program file (I think)

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,15 @@
-using System;
+/*
+This program is for memorizing scriptures by taking out a few
+words at a time until all of the words are hidden.
+*/
 
+using System;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop03 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Welcome to the scripture memorizer!");
+    Menu menu = new Menu();
+    menu.RunMenu();
+  }
 }
