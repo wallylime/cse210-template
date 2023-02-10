0dfe2bcff8945146633592cfab3c7204e800dc2d
wallylime <82895781+wallylime@users.noreply.github.com>
2023-02-10 10:56:50 -0700

    final changes to word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,41 +1,49 @@
 /*
-This part of the program deals with the details of each word,
-such as hiding it and knowing whether it is hidden.
+This part of the program deals with the details of each individual word
 */
 using System;
 
-public class Word {
+public class Word
+{
   private string _word;
-  /*
-  Giving _isHidden a value of false to begin with since I will
-  want all of the words to be displayed initially
-  */
+  /*all words should be displayed initially
+  so setting below bool to false*/
   private bool _isHidden = false;
-  public Word(string word) {
+  /*
+  Constructor below has a word parameter,
+  eliminating the need for a word setter
+  */
+  public Word(string word)
+  {
     _word = word;
   }
-  public string GetWord() {
+  public void SetIsHidden(bool isHidden) {
+    _isHidden = isHidden;
+  }
+  public bool GetIsHidden()
+  {
+    return _isHidden;
+  }
+  public string GetWord()
+  {
     return _word;
   }
-  // public void SetHidden(bool hidden) {
-  //   _isHidden = hidden;
-  // }
-public bool GetHidden() {
-  return _isHidden;
-}
-  public string HideWord() {
+  public string HideWord()
+  {
     string hiddenWord = "";
-    for (int i=0; i < _word.Length; i++) {
+    for (int i = 0; i < _word.Length; i++)
+    {
       char character = _word[i];
       bool letter = Char.IsLetter(character);
-      if (letter) {
+      if (letter)
+      {
         hiddenWord += "_";
       }
-      else {
+      else
+      {
         hiddenWord += character;
       }
     }
-    _isHidden = true;
     return hiddenWord;
   }
 }
