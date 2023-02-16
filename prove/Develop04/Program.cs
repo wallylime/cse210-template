2b9be07f29c72f13aeea6a48a4bfab780a8fa13a
wallylime <82895781+wallylime@users.noreply.github.com>
2023-02-16 13:27:26 -0700

    menu options

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,8 +2,53 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    bool quit = false;
+    Console.WriteLine("Welcome to the Improving Mindfulness program.");
+    while (quit == false)
     {
-        Console.WriteLine("Hello Develop04 World!");
+      DisplayMenu();
+      ProcessUserChoice();
     }
+
+    void DisplayMenu()
+    {
+      Console.WriteLine("\nHere are the menu options:");
+      Console.WriteLine("1. Start breathing activity");
+      Console.WriteLine("2. Start reflecting activity");
+      Console.WriteLine("3. Start listing activity");
+      Console.WriteLine("4. Quit");
+    }
+
+    void ProcessUserChoice()
+    {
+      Console.WriteLine("Please type the number that corresponds to your menu choice:");
+      string userChoice = Console.ReadLine();
+      switch (userChoice)
+      {
+        case "1":
+          Console.WriteLine("This will eventually run the breathing activity.");
+          break;
+
+        case "2":
+          Console.WriteLine("This will eventually run the reflecting activity.");
+          break;
+
+        case "3":
+          Console.WriteLine("This will eventually run the listing activity.");
+          break;
+
+        case "4":
+          quit = true;
+          break;
+
+        default:
+          Console.WriteLine("Sorry, that is not a valid response.");
+          Console.WriteLine("Please type a number 1-4 to select one of the menu options.");
+          break;
+      }
+
+    }
+  }
 }
